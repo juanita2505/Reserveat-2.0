0d6dc3d541acd1ef88d34b93a1d915aa49d9ba88
juanita2505 <jiuanitaruiz252004@gmail.com>
2025-05-23 21:52:09 -0500

    cambios

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,10 +3,8 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  standalone: true,  
+  imports: [RouterOutlet, NavbarComponent],  
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'frontend';
-}
+export class AppComponent {}
