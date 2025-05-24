e80ec5399fb87b4b6a4df23f3f41b73c022fcf2d
juanita2505 <jiuanitaruiz252004@gmail.com>
2025-05-23 21:37:58 -0500

    creacion estructura front

---
+++ frontend/src/app/restaurants/restaurant-detail/restaurant-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurant-detail',
+  imports: [],
+  templateUrl: './restaurant-detail.component.html',
+  styleUrl: './restaurant-detail.component.scss'
+})
+export class RestaurantDetailComponent {
+
+}
---
+++ frontend/src/app/restaurants/restaurant-form/restaurant-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurant-form',
+  imports: [],
+  templateUrl: './restaurant-form.component.html',
+  styleUrl: './restaurant-form.component.scss'
+})
+export class RestaurantFormComponent {
+
+}
