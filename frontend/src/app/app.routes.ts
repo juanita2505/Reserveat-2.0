3a63b1f8e234fbe6f4507c6dc657651b69c4d863
Samuelfdezurrea <est.samueld.fernan1@unimilitar.edu.co>
2025-05-25 00:21:38 -0500

    Guard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,12 +4,20 @@ import { AuthGuard } from './core/guards/auth.guard';
 export const routes: Routes = [
   {
     path: 'login',
-    loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent)
+    loadComponent: () =>
+      import('./auth/login/login.component').then(m => m.LoginComponent)
   },
   {
     path: 'restaurants',
-    loadComponent: () => import('./restaurants/restaurant-list/restaurant-list.component').then(m => m.RestaurantListComponent),
+    loadComponent: () =>
+      import('./restaurants/restaurant-list/restaurant-list.component').then(
+        m => m.RestaurantListComponent
+      ),
     canActivate: [AuthGuard]
   },
-  { path: '', redirectTo: '/restaurants', pathMatch: 'full' }
+  // Redirección inicial a login
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+
+  // Ruta comodín por si acceden a una ruta no existente
+  { path: '', redirectTo: '/login' }
 ];
